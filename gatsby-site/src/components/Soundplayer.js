d25a2e5092b1cb94be05009cd30abe1f9f351f96
djc74 <davidcooper974@gmail.com>
2018-02-12 21:30:02 +0000

    charity sample added

--- gatsby-site/src/components/Soundplayer.js
+++ gatsby-site/src/components/Soundplayer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
-import mp3_file from '../sounds/BachGavotteShort.mp3'
+import mp3_file from '../sounds/charity_upbeat.wav'
 
 const Soundplayer = () => (
   <AudioPlayer
